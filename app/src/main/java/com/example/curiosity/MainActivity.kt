af545fd97c27d3500f1d58cb02b2594e315502b7
Tia <mattyx.95@libero.it>
2022-05-14 12:04:47 +0200

    prima bozza grafica layout activity "sign up"

--- app/src/main/java/com/example/curiosity/MainActivity.kt
+++ app/src/main/java/com/example/curiosity/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.curiosity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        thread(){
+
+            Log.i("MainThread","!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+        }
     }
 }
