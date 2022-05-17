4377c6437516c58fb1bec1ceb9ab4a2f15fb28c3
Tia <mattyx.95@libero.it>
2022-05-17 22:11:44 +0200

    aggiunto splash screen

--- app/src/main/java/com/example/curiosity/MainActivity.kt
+++ app/src/main/java/com/example/curiosity/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.curiosity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.FragmentManager
-import kotlin.concurrent.thread
-import com.example.curiosity.LoginFragment as LoginFragment
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
--- app/src/main/java/com/example/curiosity/RegisterFragment.kt
+++ app/src/main/java/com/example/curiosity/RegisterFragment.kt
@@ -1,10 +1,13 @@
 package com.example.curiosity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_login.view.*
+import kotlinx.android.synthetic.main.fragment_register.view.*
 
 
 class RegisterFragment : Fragment() {
@@ -15,7 +18,19 @@ class RegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        val view = inflater!!.inflate(R.layout.fragment_register, container, false)
+
+        view.buttonRegister.setOnClickListener(){
+
+            val transaction = parentFragmentManager.beginTransaction();
+            transaction.replace(R.id.fragmentContainerView,LoginFragment())
+            transaction.commit()
+
+            Log.i("say" , "something")
+        }
+
+        // Return the fragment view/layout
+        return view
     }
 
 }
