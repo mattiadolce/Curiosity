d1bc9c40489b620c2d3cfe641e2357ab9871c733
Tia <mattyx.95@libero.it>
2022-05-19 20:58:35 +0200

    aggiunto il navigation drawer ( bozza )

--- app/src/main/java/com/example/curiosity/MainActivity.kt
+++ app/src/main/java/com/example/curiosity/MainActivity.kt
@@ -1,19 +1,51 @@
 package com.example.curiosity
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
+import android.util.Log
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var actionBar : ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         val transaction = supportFragmentManager.beginTransaction();
         transaction.replace(R.id.fragmentContainerView,LoginFragment())
+
+        val drawerLayout : DrawerLayout = findViewById( R.id.drawer_layout)
+        val navView  : NavigationView = findViewById( R.id.nav_view)
+
+        actionBar = androidx.appcompat.app.ActionBarDrawerToggle(this,drawerLayout,R.string.Open,R.string.Close)
+        drawerLayout.addDrawerListener(actionBar)
+        actionBar.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener(){it->
+            Log.i("ciao","ciao")
+            true
+        }
+
+
+
+        //actionbar.setHomeAsUpIndicator(R.drawable.ic_menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(actionBar.onOptionsItemSelected(item))
+        {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
