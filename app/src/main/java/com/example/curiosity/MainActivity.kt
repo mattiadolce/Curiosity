01a8c4944ce68b7564aef85a676b1f344baf7c58
cricci3 <cricci3@studenti.uninsubria.it>
2022-06-08 16:41:02 +0200

    basi authentication

--- app/src/main/java/com/example/curiosity/MainActivity.kt
+++ app/src/main/java/com/example/curiosity/MainActivity.kt
@@ -2,15 +2,14 @@ package com.example.curiosity
 
 import android.os.Bundle
 import android.util.Log
-import android.view.MenuItem
-import androidx.appcompat.app.ActionBar
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
-
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +19,50 @@ class MainActivity : AppCompatActivity() {
         transaction.replace(R.id.fragmentContainerView,LoginFragment())
 
 
-
+        auth = Firebase.auth
         //actionbar.setHomeAsUpIndicator(R.drawable.ic_menu)
     }
+
+    override fun onStart() {
+        val user =auth.currentUser
+        if(user == null){
+            //manda a view del login
+            loginUser("email", "password") //fare in modo che questi campi vengano letti da view
+            //da fare textfield.getText, passwordfield.getText
+        }
+        super.onStart()
+    }
+
+    private fun loginUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener{
+            if(it.isSuccessful){
+                Log.i("MainActivity", "Login ok")
+            }else{
+                //sposta a view della registrazione
+                Log.i("MainActivity", "Login fallito")
+                //messaggio: se non hai un account registrati
+                createUser(email, password)
+            }
+        }
+    }
+
+    private fun createUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
+            if(it.isSuccessful){
+                Log.i("MainActivity", "Registrazione ok")
+                //fa entrare in spp
+            }else{
+                Log.i("MainActivity", "Registrazione fallita")
+                finish()//non c'è nulla da fare, esci oppure riprova, da decidere
+            }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+
 
 
 }
