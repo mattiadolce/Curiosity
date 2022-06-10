6b781d4ab10ce81c8b801e2d8f840958056027ff
cricci3 <cricci3@studenti.uninsubria.it>
2022-06-10 14:49:50 +0200

    login e registration funzionanti

--- app/src/main/java/com/example/curiosity/HomeFragment.kt
+++ app/src/main/java/com/example/curiosity/HomeFragment.kt
@@ -5,13 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class HomeFragment : Fragment() {
+    private lateinit var auth: FirebaseAuth
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        auth = Firebase.auth
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
